a7ca81f0f0ee7b0ee1ac0b3aef810c15bcd137c6
Salah Eddine <64875502+salah1337@users.noreply.github.com>
2022-11-26 18:53:20 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -281,7 +281,7 @@ function showvid1() {
 
   var source = document.createElement("source");
 
-  if (currentLanguage == 0) source.setAttribute("src", "./vidz/1.mp4");
+  if (currentLanguage == 0) source.setAttribute("src", "https://media.githubusercontent.com/media/salah1337/human/master/vidz/1.mp4?token=APO6X3QFTB2APXEC5WSJ5RLDQJQC2");
   if (currentLanguage == 1) source.setAttribute("src", "https://media.githubusercontent.com/media/salah1337/human/master/vidz/1-eng.mp4?token=APO6X3W7EZWAG5G5XKOTZLDDQJPG6");
   if (currentLanguage == 2) source.setAttribute("src", "./vidz/1-ar.mp4");
 
@@ -301,8 +301,8 @@ function showvid2() {
 
   var source = document.createElement("source");
 
-  if (currentLanguage == 0) source.setAttribute("src", "./vidz/0.mp4");
-  if (currentLanguage == 1) source.setAttribute("src", "./vidz/0-en.mp4");
+  if (currentLanguage == 0) source.setAttribute("src", "https://media.githubusercontent.com/media/salah1337/human/master/vidz/0.mp4?token=APO6X3RQFR7XU3XGOPM24XLDQJQFK");
+  if (currentLanguage == 1) source.setAttribute("src", "https://media.githubusercontent.com/media/salah1337/human/master/vidz/0-en.mp4?token=APO6X3RQFR7XU3XGOPM24XLDQJQFK");
   if (currentLanguage == 2) source.setAttribute("src", "./vidz/0-ar.mp4");
 
   source.setAttribute("type", "video/mp4");
